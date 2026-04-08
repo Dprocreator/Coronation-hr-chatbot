fc0b4b75dd52d5762164ee9143c19a30211b4f2f
Dprocreator <oloturoyal@gmail.com>
2026-04-08 14:02:46 +0100

    made lots of changes

--- script.js
+++ script.js
@@ -571,7 +571,7 @@ const knowledgeBase = [
                 '<strong>Grooming</strong> \u2014 Neat hair, modest makeup, no nail art. Hijabs permitted (solid colors)',
                 '<strong>Enforcement</strong> \u2014 1st=Verbal Warning, 2nd=1st Warning Letter, 3rd=2nd Warning Letter'
             ],
-                action: 'Read Dress Code \u2192'
+            action: 'Read Dress Code \u2192'
         }
     },
     {
